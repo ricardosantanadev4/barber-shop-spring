f231c539cf143e283cc27a05eff729f936a054a6
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2025-03-27 11:58:04 -0300

    Projeto criado.

---
+++ src/main/java/com/dio/barber_shop_spring/BarberShopSpringApplication.java
@@ -0,0 +1,13 @@
+package com.dio.barber_shop_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberShopSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberShopSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/barber_shop_spring/BarberShopSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.barber_shop_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberShopSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
